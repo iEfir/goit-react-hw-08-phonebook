e0d172cab99dbe6003fd7ce8d5e76e28ef639368
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-05-29 11:53:07 +0300

    phonebook done

---
+++ src/components/AppBar/Logo/Logo.styled.js
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+export const Title = styled(Link)`
+  display: block;
+  font-size: 36px;
+  text-transform: uppercase;
+  font-weight: 900;
+  letter-spacing: 2px;
+
+  color: gray;
+
+  transition: color var(--main-hover-animation);
+
+  :hover,
+  :focus {
+    color: var(--accent-text-color);
+  }
+`;
+
+export const TitleColor = styled.span`
+  color: var(--accent-text-color);
+`;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { store, persistor } from 'redux/store';
+import App from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import './styles/index.css';
+import Loader from 'components/Loader';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <PersistGate loading={<Loader />} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
